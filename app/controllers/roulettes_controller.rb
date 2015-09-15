04304215003ade9ad667fc135a700a1e51af0206
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-09-15 16:10:17 -0400

    adding link to roulette game

--- app/controllers/roulettes_controller.rb
+++ app/controllers/roulettes_controller.rb
@@ -1,2 +1,6 @@
 class RoulettesController < ApplicationController
+
+  def home
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-
+  get 'roulettes/home'
   root "home#home"
-
   devise_for :users
 end
